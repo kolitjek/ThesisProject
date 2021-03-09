ced7170ce5876b05c4048f1ce8545c2941a708f3
klem95 <enxbox@hotmail.com>
2021-03-09 10:43:23 +0100

    Remerged

--- Agents/mcts_agent.py
+++ Agents/mcts_agent.py
@@ -1,12 +1,33 @@
 from Agents.agent import Agent
-from Agents.utils import MCTSHeuristic, GameStateNode
-
-class MCTSAgent (Agent):
+from aiThesis.game_state_node import GameStateNode
+#from aiThesis.game_session import GameSession
+import random
+from fireplace import utils
+class MCTSAgent(Agent):
+	currentGameState = None  # only for quick test
+	nodeCount = 0
 
 	def __init__(self, _player):
 		self.player = _player
+		self.gameTree = []
+		self.rootNode = None
 
 	def play_turn(self):
+		rootNode = GameStateNode(MCTSAgent.nodeCount)
+		rootNode.print_local_relations()
+
+		print("___________________________________")
+
+		print("Playing with MCTS agent")
+		if MCTSAgent.currentGameState is not None:
+			print("Game turn: " + str(MCTSAgent.currentGameState.turn))
+		print("___________________________________")
+		print("Actionable entities: ")
+		for entety in self.player.actionable_entities:
+			print(entety)
+
+
+	def construct_tree(self, game):
 		pass
 
 	def select_node(self):
---
+++ aiThesis/game_state_node.py
@@ -0,0 +1,28 @@
+class GameStateNode:
+	def __init__(self, _id, _parent=None):
+		self.id = _id
+		self.depthLevel = _id
+		self.game = None
+		self.parent = _parent
+		self.actionSpace = []
+		self.exploredNodes = []
+		self.leaf = False
+		self.numberOfVisits = 0
+		self.numberOfWins = 0
+
+	def print_local_relations(self):
+		print("------- Node relations -------")
+		print("Node id: " + str(self.id) + ", depth level: " + str(self.depthLevel) + ", action space:" + str(len(self.actionSpace)) + ", explored nodes: " + str(len(self.exploredNodes)))
+
+		if self.parent is not None:
+			print("Parent id: " + str(self.parent.id) + ", parent depth level: " + str(self.parent.depthLevel))
+		else:
+			print("Root Node")
+
+		print("** Explored children Nodes **")
+
+		for node in self.exploredNodes:
+			print("child id: " + str(node.id) + ", depth level: " + str(node.depthLevel) + ", action space:" +
+				str(len(self.actionSpace)) + ", explored nodes: " + str(len(self.exploredNodes)))
+
+		print("-------------------------------")
