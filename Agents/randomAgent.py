aaa963220cace00e97eb9cd2992187a588303164
Kolitjek <anton.o.sandberg@hotmail.com>
2021-03-30 12:40:17 +0200

    updated random agent to perform better

--- Agents/randomAgent.py
+++ Agents/randomAgent.py
@@ -12,19 +12,13 @@ class RandomAgent(Agent):
 
 	def play_turn(self):
 		while True:
-			heropower = self.player.hero.power
-			if heropower.is_usable() and random.random() < 0.1:
-				if heropower.requires_target():
-					heropower.use(target=random.choice(self.player.opponent.characters))
-				else:
-					heropower.use()
-				continue
+
 
 			# iterate over our hand and play whatever is playable
 			played_cards = 0
 			for card_index in range(0, len(self.player.hand)):
 				c = self.player.hand[card_index - played_cards]  #FIXME this seems to break (out of range)?
-				if c.is_playable() and random.random() < 0.5:
+				if c.is_playable(): # removed 50% chance to skip // and random.random() < 0.5
 					target = None
 					played_cards += 1
 					if c.must_choose_one:
@@ -41,6 +35,14 @@ class RandomAgent(Agent):
 						choice = random.choice(self.player.choice.cards)
 						self.player.choice.choose(choice)
 					#continue
+
+			heropower = self.player.hero.power
+			if heropower.is_usable(): #and random.random() < 0.1:
+				if heropower.requires_target():
+					heropower.use(target=random.choice(self.player.opponent.characters))
+				else:
+					heropower.use()
+
 			# Randomly attack with whatever can attack
 			for character in self.player.characters:
 				if character.can_attack():
