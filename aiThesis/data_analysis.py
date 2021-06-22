5ca7f88413458f3674fe8c5220f68632cc5ded9c
klem95 <enxbox@hotmail.com>
2021-06-22 16:24:12 +0200

    dual analysis added

--- aiThesis/data_analysis.py
+++ aiThesis/data_analysis.py
@@ -1,64 +1,84 @@
-from aiThesis.load_data import load_win_rate,warrior_base_path,warrior_filtered_path,mcts_single,mcts_sequence,priest_base_path,priest_filtered_path
+from aiThesis.load_data import load_win_rate, warrior_base_path, warrior_filtered_path, mcts_single, mcts_sequence, \
+	priest_base_path, priest_filtered_path
 import sys
 import matplotlib.pyplot as plt
 from scipy.stats import chi2_contingency
 import pandas as pd
 
 sys.path.append("")
-itrs = ["1","10","25", "50","100","200","300","400","500","800","1000"]
+itrs = ["1", "10", "25", "50", "100", "200", "300", "400", "500", "800", "1000"]
+
+
 def main():
-	w_vs_w_single_action = prepare_win_rate_analysis(load_win_rate(warrior_base_path,mcts_single),load_win_rate(warrior_filtered_path,mcts_single))
-	full_analysis(w_vs_w_single_action,"Warrior single action")
+	# w_vs_w_single_action = prepare_win_rate_analysis(load_win_rate(warrior_base_path,mcts_single),load_win_rate(warrior_filtered_path,mcts_single))
+	# full_analysis(w_vs_w_single_action,"Warrior single action")
 
 	p_vs_p_single_action = prepare_win_rate_analysis(load_win_rate(priest_base_path, mcts_single),
-													 load_win_rate(priest_filtered_path, mcts_single))
-	full_analysis(p_vs_p_single_action,"Priest single action")
+													 load_win_rate(priest_filtered_path, mcts_single), 'single')
+	p_vs_p_sequential_action = prepare_win_rate_analysis(load_win_rate(priest_base_path, mcts_sequence),
+														 load_win_rate(priest_filtered_path, mcts_sequence), 'sequence')
+
+	full_analysis(p_vs_p_single_action, p_vs_p_sequential_action, "Priest")
 	plt.show()
 
 
+def full_analysis(data_single, data_sequential, hero_type):
+	data_type_list = ['Baseline_single', 'Filtered_single', 'Baseline_sequence', 'Filtered_sequence']
+	merged_data = data_single
+	merged_data = merged_data.merge(data_sequential)
+	merged_data.plot(x='Iterations', y=data_type_list, kind='line')
+	plt.title(hero_type + ' win rate')
+	plt.ylabel('Wins')
+	plt.ylim(ymax=100, ymin=0)
 
-def full_analysis(data, plot_title):
-	data.plot(x='Iterations', y=['Baseline', 'Filtered'], kind='line')
-	plt.title(plot_title)
+	segmented_data_single = segment_data(data_single, 'single')
+	segmented_data_sequence = segment_data(data_sequential, 'sequence')
+	print(segmented_data_single)
+	print(segmented_data_sequence)
 
-	segmented_data =segment_data(data)
-	create_table(['Baseline', 'Filtered'], [itrs[i] for i in range(len(itrs))],
-				 [data['Baseline'].tolist(), data['Filtered'].tolist()],
-				 'Win rate (baseline and filtered), ' + plot_title)
-	chi2_results = calculate_chi_statistic(segmented_data)
-	create_table([itrs[i] for i in range(len(itrs))], ['dof','chi2','p'],
+	create_table(data_type_list, [itrs[i] for i in range(len(itrs))],
+				 [data_single['Baseline_single'].tolist(),
+				  data_single['Filtered_single'].tolist(),
+				  data_sequential['Baseline_sequence'].tolist(),
+				  data_sequential['Filtered_sequence'].tolist()],
+				 'Win rate (baseline and filtered), ' + hero_type)
+
+	chi2_results = calculate_chi_statistic(segmented_data_single)
+	create_table([itrs[i] for i in range(len(itrs))], ['dof', 'chi2', 'p'],
 				 chi2_results,
-				 'Chi Squared results (per iteration), ' + plot_title)
+				 'Chi Squared results (per iteration), single ' + hero_type)
 
-def prepare_win_rate_analysis (baseline_data, filtered_data):
-	baseline_data = baseline_data.rename(columns = {'Unnamed: 0':'Iterations', 'win rate':'Baseline'},inplace=False)
-	filtered_data = filtered_data.rename(columns  = {'Unnamed: 0':'Iterations', 'win rate':'Filtered'},inplace=False)
-	filtered_data_extracted = filtered_data["Filtered"]
+	chi2_results = calculate_chi_statistic(segmented_data_sequence)
+	create_table([itrs[i] for i in range(len(itrs))], ['dof', 'chi2', 'p'],
+				 chi2_results,
+				 'Chi Squared results (per iteration), sequence, ' + hero_type)
+
+
+
+def prepare_win_rate_analysis(baseline_data, filtered_data, type):
+	baseline_data = baseline_data.rename(columns={'Unnamed: 0': 'Iterations', 'win rate': 'Baseline_' + type},
+										 inplace=False)
+	filtered_data = filtered_data.rename(columns={'Unnamed: 0': 'Iterations', 'win rate': 'Filtered_' + type},
+										 inplace=False)
+	filtered_data_extracted = filtered_data['Filtered_' + type]
 	merged_table = baseline_data
 	return merged_table.join(filtered_data_extracted)
 
-def segment_data (data):
-	chi_ready_table = [[[0 for k in range(2)] for j in range(2)] for i in range(len(data))]
+
+def segment_data(data, data_type):
+	data_table = [[[0 for k in range(2)] for j in range(2)] for i in range(len(data))]
 	total_samples = 100
 	count = 0
 	for index, row in data.iterrows():
-		chi_ready_table[count][0][0] = row['Baseline']
-		chi_ready_table[count][0][1] = total_samples - row['Baseline']
-		chi_ready_table[count][1][0] = row['Filtered']
-		chi_ready_table[count][1][1] = total_samples - row['Filtered']
-		count+= 1
-	return chi_ready_table
+		data_table[count][0][0] = row['Baseline_' + data_type]
+		data_table[count][0][1] = total_samples - row['Baseline_' + data_type]
+		data_table[count][1][0] = row['Filtered_' + data_type]
+		data_table[count][1][1] = total_samples - row['Filtered_' + data_type]
+		count += 1
+	return data_table
+
 
 def create_table(x_axis, y_axis, data, label):
-
-	print(x_axis)
-	print(y_axis)
-
-	print(len(x_axis))
-	print(len(y_axis))
-	print(data)
-	print(len(data))
-	print(len(data[0]))
 	fig, ax = plt.subplots(1)
 	ax.set_axis_off()
 	table = ax.table(
@@ -70,7 +90,8 @@ def create_table(x_axis, y_axis, data, label):
 		cellLoc='center',
 		loc='upper left')
 
-	ax.set_title(label,fontweight="bold")
+	ax.set_title(label, fontweight="bold")
+
 
 def calculate_chi_statistic(data):
 	count = 0
@@ -78,7 +99,7 @@ def calculate_chi_statistic(data):
 
 	for data_sample in data:
 		chi2, p, dof, expected = chi2_contingency(data_sample)
-		results = [dof,round(chi2, 2), round(p, 2)]
+		results = [dof, round(chi2, 2), round(p, 2)]
 		collected_results.append(results)
 		count += 1
 
@@ -87,4 +108,3 @@ def calculate_chi_statistic(data):
 
 if __name__ == "__main__":
 	main()
-
