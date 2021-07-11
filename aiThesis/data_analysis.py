49476bbda88b0d0e74733f2ac4abbd865dcb833f
klem95 <enxbox@hotmail.com>
2021-07-11 09:39:39 +0200

    full itr evaluation added

--- aiThesis/data_analysis.py
+++ aiThesis/data_analysis.py
@@ -1,43 +1,60 @@
 from aiThesis.load_data import load_win_rate, warrior_base_path,hunter_base_path,hunter_filtered_path, warrior_filtered_path, mcts_single, mcts_sequence, \
-	priest_base_path, priest_filtered_path
+	priest_base_path, priest_filtered_path,druid_base_path,druid_filtered_path
 import sys
 import matplotlib.pyplot as plt
 from scipy.stats import chi2_contingency
 import pandas as pd
 
 sys.path.append("")
-itrs = ["1", "10", "25", "50", "100", "200", "300", "400", "500", "800", "1000"]
+itrs = ["1", "10", "25", "50", "100", "200", "300", "400", "500", "800"]
 mcts_type_si = 'Si'
 mcts_type_se = 'Se'
 
 def main():
+	#internal_itr_evaluation()
+	model_vs_model_evaluation()
 
+def model_vs_model_evaluation():
+	pass
 
-	w_vs_w_single_action = prepare_win_rate_analysis(load_win_rate(warrior_base_path, mcts_single),
-													 load_win_rate(warrior_filtered_path, mcts_single), mcts_type_si)
-	w_vs_w_sequential_actions = prepare_win_rate_analysis(load_win_rate(warrior_base_path,mcts_sequence),
-														  load_win_rate(warrior_filtered_path,mcts_sequence),mcts_type_se)
+def internal_itr_evaluation ():
+	'''
+		w_vs_w_single_action = prepare_win_rate_analysis(load_win_rate(warrior_base_path, mcts_single),
+														 load_win_rate(warrior_filtered_path, mcts_single), mcts_type_si)
+		w_vs_w_sequential_actions = prepare_win_rate_analysis(load_win_rate(warrior_base_path,mcts_sequence),
+															  load_win_rate(warrior_filtered_path,mcts_sequence),mcts_type_se)
+		full_analysis(w_vs_w_single_action,w_vs_w_sequential_actions,'Warrior')
+		'''
+	h_vs_h_single_action = prepare_win_rate_analysis(load_win_rate(hunter_base_path, mcts_single),
+													 load_win_rate(hunter_filtered_path, mcts_single), mcts_type_si)
+	h_vs_h_sequential_actions = prepare_win_rate_analysis(load_win_rate(hunter_base_path, mcts_sequence),
+														  load_win_rate(hunter_filtered_path, mcts_sequence),
+														  mcts_type_se)
 
-	full_analysis(w_vs_w_single_action,w_vs_w_sequential_actions,'Warrior')
+	full_analysis(h_vs_h_single_action, h_vs_h_sequential_actions, 'Hunter')
 
 	'''
 	p_vs_p_single_action = prepare_win_rate_analysis(load_win_rate(priest_base_path, mcts_single),
-													 load_win_rate(priest_filtered_path, mcts_single), 'Si')
+													 load_win_rate(priest_filtered_path, mcts_single), mcts_type_si)
 	p_vs_p_sequential_action = prepare_win_rate_analysis(load_win_rate(priest_base_path, mcts_sequence),
-														 load_win_rate(priest_filtered_path, mcts_sequence), 'Se')
+														 load_win_rate(priest_filtered_path, mcts_sequence), mcts_type_se)
 
-	h_vs_h_single_action = prepare_win_rate_analysis(load_win_rate(hunter_base_path,mcts_single),
-													 load_win_rate(hunter_filtered_path,mcts_single),'Si')
-	full_analysis(h_vs_h_single_action,[],'Hunter')
+	full_analysis(p_vs_p_single_action,p_vs_p_sequential_action,'Priest')
+	'''
 	'''
 
+	d_vs_d_single_action = prepare_win_rate_analysis(load_win_rate(druid_base_path,mcts_single),
+													 load_win_rate(druid_filtered_path,mcts_single),mcts_type_si)
 
-	#full_analysis(p_vs_p_single_action, p_vs_p_sequential_action, "Priest")
+	d_vs_d_sequential_action = prepare_win_rate_analysis(load_win_rate(druid_base_path, mcts_sequence),
+													 load_win_rate(druid_filtered_path, mcts_sequence), mcts_type_se)
+	full_analysis(d_vs_d_single_action,d_vs_d_sequential_action,'Druid')
+	'''
 	plt.show()
 
 
 def full_analysis(data_single, data_sequential, hero_type):
-	data_type_list = ['MCTS_SiN', 'MCTS_SiF', 'MCTS_SeN', 'MCTS_SeF']
+	data_type_list = ['MCTS_SiN', 'MCTS_SiP', 'MCTS_SeN', 'MCTS_SeP']
 	#data_type_list = ['MCTS_SiN', 'MCTS_SiF']
 
 	merged_data = data_single
@@ -46,6 +63,7 @@ def full_analysis(data_single, data_sequential, hero_type):
 
 	#data_single.plot(x='Iterations', y=data_type_list, kind='line')
 	plt.title(hero_type + ' win rate')
+	plt.legend(fontsize="x-large")
 	plt.ylabel('Wins')
 	plt.ylim(ymax=100, ymin=0)
 
@@ -66,9 +84,9 @@ def full_analysis(data_single, data_sequential, hero_type):
 
 	segmented_data_single = segment_data(data_single, mcts_type_si)
 	segmented_data_sequence = segment_data(data_sequential, mcts_type_se)
-	print(segmented_data_single)
-	print(segmented_data_sequence)
-	print(data_single.head())
+	#print(segmented_data_single)
+	#print(segmented_data_sequence)
+	#print(data_single.head())
 
 	create_table(data_type_list, [itrs[i] for i in range(len(itrs))],
 				 [data_single[data_type_list[0]].tolist(),
@@ -79,6 +97,7 @@ def full_analysis(data_single, data_sequential, hero_type):
 
 	chi2_results = calculate_chi_statistic(segmented_data_single)
 
+	print(len(chi2_results))
 	create_table([itrs[i] for i in range(len(itrs))], ['dof', 'chi2', 'p','Reject H0'],
 				 chi2_results,
 				 'Chi Squared results (per iteration), single ' + hero_type)
@@ -87,6 +106,7 @@ def full_analysis(data_single, data_sequential, hero_type):
 	create_table([itrs[i] for i in range(len(itrs))], ['dof', 'chi2', 'p','Reject H0'],
 				 chi2_results,
 				 'Chi Squared results (per iteration), sequence, ' + hero_type)
+	print(len(chi2_results))
 
 	'''
 	'''
@@ -95,9 +115,9 @@ def full_analysis(data_single, data_sequential, hero_type):
 def prepare_win_rate_analysis(baseline_data, filtered_data, type):
 	baseline_data = baseline_data.rename(columns={'Unnamed: 0': 'Iterations', 'win rate': 'MCTS_' + type+'N'},
 										 inplace=False)
-	filtered_data = filtered_data.rename(columns={'Unnamed: 0': 'Iterations', 'win rate': 'MCTS_' + type+'F'},
+	filtered_data = filtered_data.rename(columns={'Unnamed: 0': 'Iterations', 'win rate': 'MCTS_' + type+'P'},
 										 inplace=False)
-	filtered_data_extracted = filtered_data['MCTS_' + type+'F' ]
+	filtered_data_extracted = filtered_data['MCTS_' + type+'P' ]
 	merged_table = baseline_data
 	return merged_table.join(filtered_data_extracted)
 
@@ -108,15 +128,15 @@ def segment_data(data, data_type):
 	count = 0
 	#print(data.head())
 	for index, row in data.iterrows():
-		if  row['MCTS_' + data_type+'N'] >= 50 and row['MCTS_' + data_type+'F'] >= 50:
+		if  row['MCTS_' + data_type+'N'] >= 50 and row['MCTS_' + data_type+'P'] >= 50:
 			data_table[count][0][0] = row['MCTS_' + data_type+'N']
 			data_table[count][0][1] = total_samples - row['MCTS_' + data_type+'N']
-			data_table[count][1][0] = row['MCTS_' + data_type+'F']
-			data_table[count][1][1] = total_samples - row['MCTS_' + data_type+'F']
+			data_table[count][1][0] = row['MCTS_' + data_type+'P']
+			data_table[count][1][1] = total_samples - row['MCTS_' + data_type+'P']
 
 		count += 1
 
-	print(len(data_table))
+	#print(len(data_table))
 
 	return data_table
 
